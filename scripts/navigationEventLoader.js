3cfad6850088f2e246c17fcef9165550018d2ef2
Joshua Stahl <47959058+joshuadstahl@users.noreply.github.com>
2026-04-18 13:33:06 -0700

    cleaning up navigation event loader to use only one event listener.

--- scripts/navigationEventLoader.js
+++ scripts/navigationEventLoader.js
@@ -4,18 +4,11 @@ navigation.addEventListener('navigate', (event) => {
         blockSidebar();
         removeOverlays();
     }
-});
-
-//listens for page navigation changes.
-navigation.addEventListener('navigate', (event) => {
-    if (event.destination.url.includes("youtube.com/shorts")) {
+    else if (event.destination.url.includes("youtube.com/shorts")) {
         removeShortsContent();
     }
-});
-
-navigation.addEventListener('navigate', (event) => {
-    console.log("Navigation event detected: ", event.destination.url);
-    if (event.destination.url.includes("youtube.com/feed/") || event.destination.url == "https://youtube.com/" || event.destination.url == "https://www.youtube.com/" || event.destination.url == "https://youtube.com" || event.destination.url == "https://www.youtube.com") {
+    else if (event.destination.url.includes("youtube.com/feed/") || event.destination.url == "https://youtube.com/" || event.destination.url == "https://www.youtube.com/" || event.destination.url == "https://youtube.com" || event.destination.url == "https://www.youtube.com") {
         removeHompageContent();
     }
+
 });
